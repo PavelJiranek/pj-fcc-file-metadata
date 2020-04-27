31656e2a901623969063f2c63314ce9963e5fbb4
Pavel Jiranek <pavel.jiranek@commity.cz>
2020-04-27 16:39:04 +0200

    feat(server): return file metadata on form submit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const cors = require('cors');
 
 // require and use "multer"...
@@ -8,6 +9,8 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
+app.use(fileUpload({}));
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 // log requests
@@ -20,8 +23,11 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const FILE_INPUT_NAME = 'upfile';
+
 app.post('/api/fileanalyse', function (req, res) {
-    res.json({ greetings: "Hello, API" }); // todo analyze file
+    const { [FILE_INPUT_NAME]: { name, size, mimetype } } = req.files || { [FILE_INPUT_NAME]: {} };
+    res.json({ name, size, type: mimetype });
 });
 
 // Not found
