440ff428dfcec793414e2c8c413d5f327cd205aa
Pavel Jiranek <pavel.jiranek@commity.cz>
2020-04-27 16:10:53 +0200

    feat(server): basic routing

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
 'use strict';
 
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 
 // require and use "multer"...
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
+// log requests
+app.use(({ method, path }, res, next) => {
+    console.log(`${method} ${path}`);
+    next();
+});
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', function (req, res) {
+    res.json({ greetings: "Hello, API" }); // todo analyze file
+});
+
+// Not found
+app.get('*', function (req, res) {
+    res.status(404).send("No file here");
 });
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+    console.log('Node.js listening ...');
 });
